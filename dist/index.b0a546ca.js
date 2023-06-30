cd8793511a8b5438eaf044edfcada9d04961a88f
Sujugn <pepsi030@naver.com>
2023-06-30 16:12:55 +0900

    d

---
+++ dist/index.b0a546ca.js
@@ -0,0 +1,25 @@
+var mySwiper = new Swiper(".swiper-container", {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000
+    },
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 80,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
+
+//# sourceMappingURL=index.b0a546ca.js.map
